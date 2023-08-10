b820de84799f0a0ed5bdd2c181c980d35b471c64
ajay sieunarine <ajay_sieunarine@hotmail.com>
2023-08-10 16:08:45 -0400

    start portfolio item comp tek

---
+++ src/components/PortfolioItem.js
@@ -0,0 +1,25 @@
+const PortfolioItem = ({ category, title, desc, imgSrc, projectLink }) => {
+  return (
+      <div className={`grid-item product ${category}`}>
+        <div className="portfolio-box-01">
+          <div className="portfolio-info">
+            <h5 className="white-color font-weight-bold">{title}</h5>
+            <span>{desc}</span>
+          </div>
+          <div className="portfolio-img">
+            <img src={imgSrc} title={title} alt={title} />
+            <div className="portfolio-icon">
+              <a
+                  href={projectLink}
+                  className="gallery-link"
+                  target="_blank" rel="noopener noreferrer"
+              >
+                <span className="ti-plus" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+export default PortfolioItem;
